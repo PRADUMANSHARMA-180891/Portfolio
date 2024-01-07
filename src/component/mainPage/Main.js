759a55ab3cfdc535653fc70e66a5f2ac352fd524
PRADUMANSHARMA-180891 <praduman.cuh@gmail.com>
2024-01-07 19:14:45 +0530

    button of landing page modify

--- src/component/mainPage/Main.js
+++ src/component/mainPage/Main.js
@@ -8,24 +8,24 @@ const Main = () => {
       <div className='md:mr-64 text-white md:text-center text-center'>
         <h1 className='my-4 font-general-semibold text-2xl lg:text-3xl xl:text-4xl text-center sm:text-left text-ternary-dark dark:text-primary-light uppercase'> HI, IAM PRADUMAN</h1>
         <p className='font-serif text-2xl '>A Full-Stack Developer Engineer</p>
-        <button type="button" class="
+        <button type="button" className="
         text-gray-900 bg-white border
          border-gray-300 
          focus:outline-none
-          hover:bg-gray-100 
+          hover:bg-gray-200 
           focus:ring-4
            focus:ring-gray-200 
            font-medium rounded-lg
             text-sm px-5 
             py-2.5 
             me-2 
-            mb-2
+            mt-8
              dark:bg-gray-800
               dark:text-white
          dark:border-gray-600
           dark:hover:bg-gray-700
            dark:hover:border-gray-600
-           dark:focus:ring-gray-700">Light</button>
+           dark:focus:ring-gray-700">Download CV</button>
       </div>
       <div className='md:w-1/3 md:h-1/3 mx-10 my-10 w-full h-1/2'>
         <img src='https://react-tailwindcss-portfolio.netlify.app/static/media/developer-dark.3f07bd13.svg' alt='prdy'/>
