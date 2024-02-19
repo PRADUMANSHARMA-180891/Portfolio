f0aa47d74404915accb51098c48c3f1a514399b2
PRADUMANSHARMA-180891 <praduman.cuh@gmail.com>
2024-02-19 23:56:34 +0530

    feat (GS-2): add download cv functionality

--- src/component/mainPage/Main.js
+++ src/component/mainPage/Main.js
@@ -1,14 +1,23 @@
 import React from 'react'
 import Portfolio from '../portfolio/Portfolio'
-
+const cvUrl = 'http://localhost:3000/cv.pdf';
 const Main = () => {
+  const handleDownloadCV = (url) => {
+    const fileName =url.split("/").pop();
+    const aTag =document.createElement('a');
+    aTag.href =url;
+    aTag.setAttribute("download",fileName);
+    document.body.append(aTag);
+    aTag.click();
+    aTag.remove();
+  };
   return (
   <section className='bg-grey-100 w-screen h-screen bg-slate-800'>
     <div className='flex flex-col sm:flex-row  justify-center items-center mx-auto '>
       <div className='md:mr-64 text-white md:text-center text-center'>
         <h1 className='my-4 font-general-semibold text-2xl lg:text-3xl xl:text-4xl text-center sm:text-left text-ternary-dark dark:text-primary-light uppercase'> HI, IAM PRADUMAN</h1>
         <p className='font-serif text-2xl '>A Full-Stack Developer Engineer</p>
-        <button type="button" className="text-gray-900 bg-yellow-500 borderborder-gray-300 
+        <button onClick={()=>{handleDownloadCV(cvUrl)}} type="button" className="text-gray-900 bg-yellow-500 borderborder-gray-300 
         focus:outline-nonehover:bg-yellow-600 focus:ring-4 focus:ring-gray-200 
         font-medium rounded-lg text-sm px-5 py-2.5 me-2 mt-16"
         >Download CV
--- src/component/pages/HireModel.js
+++ src/component/pages/HireModel.js
@@ -8,15 +8,18 @@ const HireModel =({isopen, setIsOpen})=>{
 
         <div className="fixed flex justify-center inset-0 mt-20">
           <div className='bg-slate-500 w-80 h-64 text-center relative rounded-md'>
-            <p>please Fill Information</p>
+            <p className='text-xl font-sans text-gray-100 mt-1'>please Fill Information</p>
             <form>
-                <div className='mx-auto mt-6'>
-                   <input type='input' className='border-1 ext-md px-2 py-1 w-56 rounded-sm' placeholder='please write contact number'/>
-                   <textarea className='mt-4 h-28 w-56 px-2 py-2 rounded-sm'/>
+                <div className='mx-auto mt-3'>
+                   <input type='input' className='bg-gray-100 border border-gray-300 text-gray-900 text-md rounded-sm focus:ring-blue-500 focus:border-blue-500 p-2.5 px-2 py-1 w-56 ' placeholder='please enter contact number'/>
+                   <textarea className='bg-gray-100 border border-gray-300 text-gray-900 text-md mt-4 h-24 w-56 px-2 py-2 rounded-sm'/>
                 </div>
             </form>
-            <div className='absolute right-2 bottom-2'>
-            <button className='' type='button' onClick={() => setIsOpen(false)}>Close Modal</button>
+            <div className='absolute left-12 bottom-2 text-white border-2 border-blue-600 bg-blue-600 hover:bg-blue-700 px-6 py-1.5 mt-4 rounded-full focus:ring-4'>
+            <button className='' type='button' onClick={() => setIsOpen(false)}>close</button>
+            </div>
+            <div className='absolute right-12 bottom-2 text-white border-2 border-blue-600 bg-blue-600 hover:bg-blue-700 px-6 py-1.5 mt-4 rounded-full focus:ring-4'>
+            <button className='' type='button'>send</button>
             </div>
           </div>
         </div>
